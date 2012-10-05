b554902bf7c25e5a8b05382be8ce5c18fed7fedb
Stefan Kluth <skluth@mpp.mpg.de>
2012-10-05 14:15:00 +0200

    setup script for cernvm

---
+++ setup-cernvm.sh
@@ -0,0 +1,9 @@
+
+# Setup root and python with gcc 43 because the build fails
+# with gcc46
+
+source /cvmfs/sft.cern.ch/lcg/external/gcc/4.3.5/x86_64-slc5-gcc34-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.00/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh 
+PYTHONHOME=/cvmfs/sft.cern.ch/lcg/external/Python/2.6.5/x86_64-slc5-gcc43-opt
+export PATH=${PYTHONHOME}/bin:${PATH}
+export LD_LIBRARY_PATH=${PYTHONHOME}/lib:${LD_LIBRARY_PATH}
