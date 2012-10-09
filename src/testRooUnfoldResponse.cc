bff28d8700cd3ccb91f13e87de49249720a8f0db
Eike Schlieckau <eike.schlieckau@desy.de>
2012-10-09 15:30:35 +0200

    added new test suite for RooUnfoldResponse: testRooUnfoldResponse.cc

---
+++ src/testRooUnfoldResponse.cc
@@ -0,0 +1,38 @@
+// Unit tests for INI file reader class
+// A. Vanhoefer, E. Schlieckau, 10/2012
+
+#include "RooUnfoldResponse.h"
+
+// BOOST test stuff:
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE RooUnfoldResponseTests
+#include <boost/test/unit_test.hpp>
+
+// Namespaces:
+using std::string;
+using namespace RooUnfoldResponse;
+
+// Test fixture for all tests:
+class RooUnfoldResponseFixture{
+public:
+  RooUnfoldResponseFixture(){
+    BOOST_MESSAGE( "Create RooUnfoldResponseFixture" );
+  }
+  virtual ~RooUnfoldResponseFixture(){
+    BOOST_MESSAGE( "Tear down RooUnfoldResponseFixture" );
+  }
+  RooUnfoldResponse response;
+};
+
+
+// Declare test suite name and fixture class to BOOST:
+BOOST_FIXTURE_TEST_SUITE( inireadersuite, INIReaderTestFixture )
+
+// Test cases:
+
+// Failing test
+BOOST_AUTO_TEST_CASE(testFails){
+  BOOST_FAIL("This test fails!");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
