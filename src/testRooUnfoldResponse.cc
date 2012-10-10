ca44aa1c023404c0f942bc1f6fed640898713196
Eike Schlieckau <eike.schlieckau@desy.de>
2012-10-10 14:20:03 +0200

    completed test of ConstructorNumberOfBins

--- src/testRooUnfoldResponse.cc
+++ src/testRooUnfoldResponse.cc
@@ -3,6 +3,8 @@
 
 #include "RooUnfoldResponse.h"
 
+#include "TH2.h"
+
 // BOOST test stuff:
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE RooUnfoldResponseTests
@@ -34,17 +36,22 @@ BOOST_FIXTURE_TEST_SUITE( RooUnfoldResponseSuite, RooUnfoldResponseFixture )
 // BOOST_FAIL("This test fails!");
 //}
 
-  BOOST_AUTO_TEST_CASE(testConstructorNumberOfBins){
+BOOST_AUTO_TEST_CASE(testConstructorNumberOfBins){
   int numberOfBins = 10;
   double low = 1;
   double high = 11;
   RooUnfoldResponse responseWithNumberOfBins(numberOfBins, low, high);
+
   int resultNumberOfBinsMeasured = responseWithNumberOfBins.GetNbinsMeasured();
   int resultNumberOfBinsTruth    = responseWithNumberOfBins.GetNbinsTruth();
   TH2* responseHistogram = responseWithNumberOfBins.Hresponse();
+  double responseHistogramLow = responseHistogram->GetXaxis()->GetBinLowEdge(1);
+  double responseHistogramHigh = responseHistogram->GetXaxis()->GetBinLowEdge(numberOfBins)+responseHistogram->GetXaxis()->GetBinWidth(numberOfBins);
   BOOST_CHECK_MESSAGE(numberOfBins == resultNumberOfBinsMeasured, "Number of bins measured not on given value: " << resultNumberOfBinsMeasured << " != " << numberOfBins);
   BOOST_CHECK_MESSAGE(numberOfBins == resultNumberOfBinsTruth, "Number of bins truth not on given value: " << resultNumberOfBinsTruth << " != " << numberOfBins);
   BOOST_CHECK_MESSAGE(resultNumberOfBinsMeasured == resultNumberOfBinsTruth, "Number of bins truth not equal to number of bins measured: " << resultNumberOfBinsTruth << " != " << resultNumberOfBinsMeasured);
+  BOOST_CHECK_MESSAGE(low == responseHistogramLow, "First bin low edge not taken correctly: " << responseHistogramLow << " != " << low);
+  BOOST_CHECK_MESSAGE(high == responseHistogramHigh, "Last bin high edge not taken correctly: " << responseHistogramHigh << " != " << high);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
