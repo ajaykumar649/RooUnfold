ce4c0f0a2a711fc9134ccc0d9b3d118a09dc8873
Henning Kirschenmann <henning.kirschenmann@desy.de>
2012-10-10 14:58:32 +0200

    test of overflowstatus getter-method

--- src/testRooUnfoldResponse.cc
+++ src/testRooUnfoldResponse.cc
@@ -26,12 +26,19 @@ public:
 
 // Declare test suite name and fixture class to BOOST:
 BOOST_FIXTURE_TEST_SUITE( RooUnfoldResponseSuite, RooUnfoldResponseFixture )
+//BOOST_AUTO_TEST_SUITE( RooUnfoldResponseSuite )
 
 // Test cases:
 
 // Failing test
 BOOST_AUTO_TEST_CASE(testFails){
   BOOST_FAIL("This test fails!");
+}
+
+//Test of UseOverflowStatus
+BOOST_AUTO_TEST_CASE(testUseOverflowStatus){
+  //  RooUnfoldResponse testObject;
+  BOOST_CHECK_MESSAGE(response.UseOverflowStatus()==false,"default constructor does not initialize with overflow set to false");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
