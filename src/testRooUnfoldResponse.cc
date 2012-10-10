6b57221acab5f984bbc611c847cde3c331f5c9b2
Eike Schlieckau <eike.schlieckau@desy.de>
2012-10-10 15:20:22 +0200

    added 'responseSameBinsMeasuredTruth' to Fixture

--- src/testRooUnfoldResponse.cc
+++ src/testRooUnfoldResponse.cc
@@ -16,14 +16,18 @@ using std::string;
 // Test fixture for all tests:
 class RooUnfoldResponseFixture{
 public:
-  RooUnfoldResponseFixture(){
+  RooUnfoldResponseFixture()
+  {
     BOOST_MESSAGE( "Create RooUnfoldResponseFixture" );
+    responseSameBinsMeasuredTruth = RooUnfoldResponse(10,0.,100.);
   }
   virtual ~RooUnfoldResponseFixture(){
     BOOST_MESSAGE( "Tear down RooUnfoldResponseFixture" );
   }
   RooUnfoldResponse response;
+  RooUnfoldResponse responseSameBinsMeasuredTruth;
 };
+
 
 
 // Declare test suite name and fixture class to BOOST:
@@ -58,6 +62,22 @@ BOOST_AUTO_TEST_CASE(testConstructorNumberOfBins){
   int truthDimensions = responseWithNumberOfBins.GetDimensionTruth();
   BOOST_CHECK_MESSAGE(measuredDimensions == 1, "Wrong measured dimension, has to be 1 but is: " << measuredDimensions);
   BOOST_CHECK_MESSAGE(truthDimensions == 1, "Wrong truth dimension, has to be 1 but is: " << truthDimensions);
+}
+
+BOOST_AUTO_TEST_CASE(testFill1D){
+  //test with default weight
+  double xMeasured = 42;
+  double xTruth = 74;
+  responseSameBinsMeasuredTruth.Fill(xMeasured,xTruth);
+  TH1* measuredHistogram = responseSameBinsMeasuredTruth.Hmeasured();
+  int entriesMeasured = measuredHistogram->GetEntries();
+  //int 
+  //BOOST_CHECK_MESSAGE();
+
+
+  TH1* truthHistogram = responseSameBinsMeasuredTruth.Htruth();
+  TH2* responseHistogram = responseSameBinsMeasuredTruth.Hresponse();
+  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
