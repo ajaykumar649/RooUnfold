b5a3eb60f3aa951757c7446c14fbbf44d284d93f
Eike Schlieckau <eike.schlieckau@desy.de>
2012-10-10 13:37:16 +0200

    started test for ConstructorNumberOfBins

--- src/testRooUnfoldResponse.cc
+++ src/testRooUnfoldResponse.cc
@@ -30,8 +30,21 @@ BOOST_FIXTURE_TEST_SUITE( RooUnfoldResponseSuite, RooUnfoldResponseFixture )
 // Test cases:
 
 // Failing test
-BOOST_AUTO_TEST_CASE(testFails){
-  BOOST_FAIL("This test fails!");
+//BOOST_AUTO_TEST_CASE(testFails){
+// BOOST_FAIL("This test fails!");
+//}
+
+  BOOST_AUTO_TEST_CASE(testConstructorNumberOfBins){
+  int numberOfBins = 10;
+  double low = 1;
+  double high = 11;
+  RooUnfoldResponse responseWithNumberOfBins(numberOfBins, low, high);
+  int resultNumberOfBinsMeasured = responseWithNumberOfBins.GetNbinsMeasured();
+  int resultNumberOfBinsTruth    = responseWithNumberOfBins.GetNbinsTruth();
+  TH2* responseHistogram = responseWithNumberOfBins.Hresponse();
+  BOOST_CHECK_MESSAGE(numberOfBins == resultNumberOfBinsMeasured, "Number of bins measured not on given value: " << resultNumberOfBinsMeasured << " != " << numberOfBins);
+  BOOST_CHECK_MESSAGE(numberOfBins == resultNumberOfBinsTruth, "Number of bins truth not on given value: " << resultNumberOfBinsTruth << " != " << numberOfBins);
+  BOOST_CHECK_MESSAGE(resultNumberOfBinsMeasured == resultNumberOfBinsTruth, "Number of bins truth not equal to number of bins measured: " << resultNumberOfBinsTruth << " != " << resultNumberOfBinsMeasured);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
