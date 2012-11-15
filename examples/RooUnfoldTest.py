9f4d75e93481fb65e2b353d85f0644fceb3357cb
Stefan Kluth <skluth@mpp.mpg.de>
2012-11-15 18:34:04 +0100

    refactoring: introduce classes so code reuse is easier

--- examples/RooUnfoldTest.py
+++ examples/RooUnfoldTest.py
@@ -17,144 +17,214 @@ TH1.SetDefaultSumw2( False )
 
 
 #  Gaussian smearing, systematic translation, and variable inefficiency:
-def smear( xt, mean=-1.0, sigma=0.05, leff=True ):
-    # efficiency
-    xeff= 0.3 + (1.0-0.3)/10.0*xt
-    x= gRandom.Rndm()
-    if leff and x > xeff:
-        return None
-    # bias and smear
-    xsmear= gRandom.Gaus( mean, sigma )
-    return xt+xsmear
 
-# Training: create response object:
-def train( bininfo, gmean=-1.0, gsigma=0.05, leff=True, loufl=False,
-           opttfun="exp" ):
-    print "============================ TRAIN ============================="
-    txt= "Smear mu, s.d.: " + str(gmean) + ", " + str(gsigma) + ", eff.: " + str(leff) + ", o/u-flow: " + str(loufl) + ", function: " + opttfun
-    print txt
-    # Create response matrix object:
-    response= RooUnfoldResponse( bininfo["mbins"],
-                                 bininfo["mlo"],
-                                 bininfo["mhi"],
-                                 bininfo["tbins"],
-                                 bininfo["tlo"],
-                                 bininfo["thi"] )
-    response.UseOverflow( loufl )
-    # Set training truth function:
-    if "exp" in opttfun:
-        trainfun= TF1( "trainexp", "exp(-x/3.0)",
-                       bininfo["tlo"], bininfo["thi"] )
-    elif "bw" in opttfun:
-        trainfun= TF1( "trainbw", "TMath::BreitWigner(x,4.0,1.0)",
-                       bininfo["tlo"], bininfo["thi"] )
-    elif "gaus" in opttfun:
-        trainfun= TF1( "traingaus", "TMath::Gaus(x,5.0,2.5)",
-                       bininfo["tlo"], bininfo["thi"] )
-    # Run training simulation:
-    for i in xrange( 100000 ):
-        xt= trainfun.GetRandom()
-        x= smear( xt, gmean, gsigma, leff )
-        if x != None:
-            response.Fill( x, xt )
-        else:
-            response.Miss( xt )
-    return response
+class Measurement:
 
-# Generate test distributions:
-def generateTest( bininfo, testfun, gmean=-1.0, gsigma=0.05, leff=True ):
-    hTrue, hMeas= makeTestHistos( bininfo )
-    for i in xrange( 10000 ):
-        xt= testfun.GetRandom()
-        hTrue.Fill( xt )
-        x= smear( xt, gmean, gsigma, leff )
-        if x != None:
-            hMeas.Fill( x )
-    return hTrue, hMeas
-def makeTestHistos( bininfo ):
-    hTrue= TH1D( "true", "Test Truth",
-                 bininfo["tbins"],
-                 bininfo["tlo"],
-                 bininfo["thi"] )
-    hMeas= TH1D( "meas", "Test Measured",
-                 bininfo["mbins"],
-                 bininfo["mlo"],
-                 bininfo["mhi"] )
-    return hTrue, hMeas
+    def __init__( self, mean, sigma, leff ):
+        self.mean= mean
+        self.sigma= sigma
+        self.leff= leff
+        return
 
-# Create unfolder object:
-def unfolderFactory( optunf, response, hMeas, BasisSpline_m0=0 ):
-    if "Bayes" in optunf:
-        # Bayes unfoldung until chi^2 cut (max 10):
-        unfold= RooUnfoldBayes( response, hMeas, 10, False, True )
-    elif "SVD" in optunf:
-        # SVD unfolding with free regularisation:
-        unfold= RooUnfoldSvd( response, hMeas )
-    elif "TUnfold" in optunf:
-        # TUnfold with fixed regularisation tau=0.002
-        unfold= RooUnfoldTUnfold( response, hMeas, 0.002 )
-        # unfold= RooUnfoldTUnfold( response, hMeas )
-    elif "Invert" in optunf:
-        unfold= RooUnfoldInvert( response, hMeas )
-    elif "Reverse" in optunf:
-        # Use equivalent Bayes with 1 iteration:
-        unfold= RooUnfoldBayes( response, hMeas, 1 )
-    elif "BasisSplines" in optunf:
-        # unfold= RooUnfoldBasisSplines( response, hMeas, 1.3e-6, 32 )
-        # unfold= RooUnfoldBasisSplines( response, hMeas, 0.0, 32, 2 )
-        # unfold= RooUnfoldBasisSplines( response, hMeas, 0.0, BasisSpline_m0, 2 )
-        unfold= RooUnfoldBasisSplines( response, hMeas, 0.0, BasisSpline_m0, 0 )
-    return unfold
+    def getMean( self ):
+        return self.mean
+    def getSigma( self ):
+        return self.sigma
+    def getLeff( self ):
+        return self.leff
 
-# Run a test of the unfolding methods:
-def rununfoldtest( bininfo, optunf, response,
-                   gmean=-1.0, gsigma=0.05,
-                   leff=True, optfun="exp" ):
-    optunfs= [ "Bayes", "SVD", "TUnfold", "Invert", "Reverse", "BasisSplines" ]
-    if not optunf in optunfs:
-        txt= "Unfolding option " + optunf + " not recognised" 
-        raise ValueError( txt )
-    print "============================= TEST =========================="
-    txt= "Method: " + optunf + ", smear mu, s.d.: " + str(gmean) + ", " + str(gsigma) + ", eff.: " +str(leff) + ", function: " + optfun
-    print txt
+    def efficiency( self, x ):
+        return 0.3 + (1.0-0.3)/10.0*x
+
+    def accept( self, x ):
+        result= True
+        if self.leff:
+            p= self.efficiency( x )
+            r= gRandom.Rndm()
+            if r > p:
+                result= False
+        return result
+
+    def smear( self, xt ):
+        xsmear= gRandom.Gaus( self.mean, self.sigma )
+        return xt+xsmear
+
+    def transform( self, x ):
+        return x
+
+    def generate( self, fun ):
+        xt= fun.GetRandom()
+        x= None
+        if self.accept( xt ):
+            x= self.smear( self.transform( xt ) )
+        return xt, x
+
+class BlobelMeasurement( Measurement ):
+    def __init__( self, mean, sigma, leff ):
+        Measurement.__init__( self, mean, sigma, leff )
+        return
+    def efficiency( self, x ):
+        return 1.0 - 0.5*(x-1.0)**2
+    def transform( self, x ):
+        return x - 0.2*x**2/4.0
+
+def createMeasurement( mean, sigma, leff, opt="" ):
+    if "blobel" in opt:
+        return BlobelMeasurement(  mean, sigma, leff )
+    else:
+        return Measurement(  mean, sigma, leff )
+
+class Trainer:
+
+    def __init__( self, bininfo, optfun="exp" ):
+        self.bininfo= bininfo
+        # Set training truth function:
+        self.optfun= optfun
+        self.trainfun= createFunction( bininfo, optfun, "train"+optfun )
+        return
+
+    # Training: create response object:
+    def train( self, measurement, loufl=False ):
+        print "============================ TRAIN ============================="
+        txt= "Smear mu, s.d.: " + str(measurement.getMean())
+        txt+= ", " + str(measurement.getSigma()) + ", eff.: "
+        txt+= str(measurement.getLeff())
+        txt+= ", o/u-flow: " + str(loufl) + ", function: " + self.optfun
+        print txt
+        # Create response matrix object:
+        response= RooUnfoldResponse( self.bininfo["mbins"],
+                                     self.bininfo["mlo"],
+                                     self.bininfo["mhi"],
+                                     self.bininfo["tbins"],
+                                     self.bininfo["tlo"],
+                                     self.bininfo["thi"] )
+        response.UseOverflow( loufl )
+        for i in xrange( 100000 ):
+            xt, x= measurement.generate( self.trainfun )
+            if x != None:
+                response.Fill( x, xt )
+            else:
+                response.Miss( xt )
+        return response
+
+
+class Tester:
+
+    def __init__( self, bininfo, optfun ):
+        self.bininfo= bininfo
+        self.optfun= optfun
+        self.testfun= createFunction( bininfo, optfun, "test"+optfun )
+        return
+
+    # Generate test distributions:
+    def generateTest( self, measurement ):
+        hTrue, hMeas= self.makeTestHistos()
+        for i in xrange( 10000 ):
+            xt, x= measurement.generate( self.testfun )
+            hTrue.Fill( xt )
+            if x != None:
+                hMeas.Fill( x )
+        return hTrue, hMeas
+
+    def makeTestHistos( self ):
+        hTrue= TH1D( "true", "Test Truth",
+                     self.bininfo["tbins"],
+                     self.bininfo["tlo"],
+                     self.bininfo["thi"] )
+        hMeas= TH1D( "meas", "Test Measured",
+                     self.bininfo["mbins"],
+                     self.bininfo["mlo"],
+                     self.bininfo["mhi"] )
+        return hTrue, hMeas
+
+
+
+class UnfoldTester:
+
+    def __init__( self, optunf ):
+        optunfs= [ "Bayes", "SVD", "TUnfold", "Invert", "Reverse",
+                   "BasisSplines" ]
+        if not optunf in optunfs:
+            txt= "UnfoldTester: Unfolding option " + optunf + " not recognised" 
+            raise ValueError( txt )
+        self.optunf= optunf
+        return
+
+    # Create unfolder object:
+    def unfolderFactory( self, response, hMeas, BasisSpline_m0=0 ):
+        if "Bayes" in self.optunf:
+            # Bayes unfoldung until chi^2 cut (max 10):
+            unfold= RooUnfoldBayes( response, hMeas, 10, False, True )
+        elif "SVD" in self.optunf:
+            # SVD unfolding with free regularisation:
+            unfold= RooUnfoldSvd( response, hMeas )
+        elif "TUnfold" in self.optunf:
+            # TUnfold with fixed regularisation tau=0.002
+            unfold= RooUnfoldTUnfold( response, hMeas, 0.002 )
+            # unfold= RooUnfoldTUnfold( response, hMeas )
+        elif "Invert" in self.optunf:
+            unfold= RooUnfoldInvert( response, hMeas )
+        elif "Reverse" in self.optunf:
+            # Use equivalent Bayes with 1 iteration:
+            unfold= RooUnfoldBayes( response, hMeas, 1 )
+        elif "BasisSplines" in self.optunf:
+            # BasisSplines with automatic tau determination
+            unfold= RooUnfoldBasisSplines( response, hMeas, 0.0, BasisSpline_m0, 2 )
+        return unfold
+
+    # Run a test of the unfolding methods:
+    def rununfoldtest( self, tester, measurement, response ):
+        print "============================= TEST =========================="
+        txt= "Method: " + self.optunf + ", smear mu, s.d.: "
+        txt+= str(measurement.getMean()) + ", "
+        txt+= str(measurement.getSigma())
+        txt+= ", eff.: " + str(measurement.getLeff())
+        txt+= ", function: " + tester.optfun
+        print txt
+        hTrue, hMeas= tester.generateTest( measurement )
+        print "=========================== UNFOLD ========================"
+        print "Unfolding method:", self.optunf
+        unfold= self.unfolderFactory( response, hMeas )
+        return unfold, hTrue, hMeas
+
+
+# Create functions for training and testing:
+def createFunction( bininfo, optfun, name ):
     if "exp" in optfun:
-        testfun= TF1( "testexp", "exp(-x/3.0)", 0.0, 10.0 )
+        fun= TF1( name, "exp(-x/3.0)",
+                  bininfo["tlo"], bininfo["thi"] )
     elif "bw" in optfun:
-        testfun= TF1( "testbw", "TMath::BreitWigner(x,4.0,1.0)", 0.0, 10.0 )
-    hTrue, hMeas= generateTest( bininfo, testfun,
-                                gmean=gmean, gsigma=gsigma, leff=leff )
-    print "=========================== UNFOLD ========================"
-    print "Unfolding method:", optunf
-    m0= 0
-    if optunf == "BasisSplines":
-        if optfun == "exp":
-            m0= 9
-        else:
-            m0= 32
-    unfold= unfolderFactory( optunf, response, hMeas, m0 )
-    return unfold, hTrue, hMeas
-
-# Create text for histo titles:
-def funtxts( opttfun, optfun, leff, loufl ):
-    if "exp" in opttfun:
-        funttxt= "Exp tau=3"
-    elif "bw" in opttfun:
-        funttxt= "B-W mu=4, s.d.=1"
-    elif "gaus" in opttfun:
-        funttxt= "Gaus mu=5, s.d.=2.5"
-    if "exp" in optfun:
-        funtxt= "Exp tau=3"
-    elif "bw" in optfun:
-        funtxt= "B-W mu=4, s.d.=1"
-    if leff:
-        funtxt+= ", eff."
-    if loufl:
-        funtxt+= ", o/u-flow"
-    return funttxt, funtxt
+        fun= TF1( name, "TMath::BreitWigner(x,4.0,1.0)",
+                  bininfo["tlo"], bininfo["thi"] )
+    elif "gaus" in optfun:
+        fun= TF1( name, "TMath::Gaus(x,5.0,2.5)",
+                  bininfo["tlo"], bininfo["thi"] )
+    elif "blobel" in optfun:
+        def blobelFun( xx ):
+            xxx= xx[0]
+            b= [ 1.0, 10.0, 5.0 ]
+            x= [ 0.4, 0.8, 1.5 ]
+            g= [ 2.0, 0.2, 0.2 ]
+            f= 0.0
+            for k in range( 3 ):
+                f+= b[k]*g[k]**2/(( xxx - x[k] )**2+g[k]**2)
+            return f
+        fun= TF1( name, blobelFun, bininfo["tlo"], bininfo["thi"] )
+    else:
+        print "Function option", optfun, "not recognised"
+        fun= None
+    return fun
 
 # Provide binning for different test functions:
 def createBininfo( optfun ):
     if "exp" in optfun:
+        bininfo= { "tbins": 10,
+                   "tlo": 0.0,
+                   "thi": 10.0,
+                   "mbins": 20,
+                   "mlo": -2.0,
+                   "mhi": 10.0 }
+    elif "gaus" in optfun:
         bininfo= { "tbins": 10,
                    "tlo": 0.0,
                    "thi": 10.0,
@@ -168,11 +238,40 @@ def createBininfo( optfun ):
                    "mbins": 80,
                    "mlo": -2.0,
                    "mhi": 10.0 }
+    elif "blobel" in optfun:
+        bininfo= { "tbins": 12,
+                   "tlo": 0.0,
+                   "thi": 2.0,
+                   "mbins": 40,
+                   "mlo": 0.0,
+                   "mhi": 2.0 }
     return bininfo
+
+# Create text for histo titles:
+def funtxts( opttfun, optfun, leff, loufl ):
+    if "exp" in opttfun:
+        funttxt= "Exp tau=3"
+    elif "bw" in opttfun:
+        funttxt= "B-W mu=4, s.d.=1"
+    elif "gaus" in opttfun:
+        funttxt= "Gaus mu=5, s.d.=2.5"
+    elif "blobel" in opttfun:
+        funttxt= "Blobel"
+    if "exp" in optfun:
+        funtxt= "Exp tau=3"
+    elif "bw" in optfun:
+        funtxt= "B-W mu=4, s.d.=1"
+    elif "blobel" in optfun:
+        funtxt= "Blobel"
+    if leff:
+        funtxt+= ", eff."
+    if loufl:
+        funtxt+= ", o/u-flow"
+    return funttxt, funtxt
 
 # Plot pull distributions:
 def plotPulls( optunf="Bayes", ntest=10, leff=True, loufl=False,
-               optfun="exp", opttfun="" ):
+               optfun="exp", opttfun="", gmean=-1.0 ):
 
     if opttfun == "":
         opttfun= optfun
@@ -185,25 +284,28 @@ def plotPulls( optunf="Bayes", ntest=10, leff=True, loufl=False,
     canv= TCanvas( "canv", "thruth vs reco pulls", 600, 800 )
     canv.Divide( 1, 3 )
 
-    gmeantrain= -1.0
-    gmeantest= gmeantrain
+    trainer= Trainer( bininfo, opttfun )
+    # trainer= BTrainer( bininfo, opttfun )
+
+    tester= Tester( bininfo, optfun )
+    unfoldtester= UnfoldTester( optunf )
     for sigma, ipad in [ [ 0.1, 1 ], [ 0.3, 2 ], [ 1.0, 3 ] ]:
         hPulls= TProfile( "pulls",
                           optunf + ", smear mu, s.d.= " +
-                          str(gmeantrain) + ", " + str(sigma) +
+                          str(gmean) + ", " + str(sigma) +
                           ", train: " + funttxt + ", test: " + funtxt +
                           ", " + str(ntest) + " tests",
                           bininfo["tbins"], bininfo["tlo"], bininfo["thi"] )
         hPulls.SetErrorOption( "s" )
         hPulls.SetYTitle( "Thruth reco pull" )
         histos.append( hPulls )
-        response= train( bininfo, gmean=gmeantrain, gsigma=sigma, leff=leff,
-                         opttfun=opttfun, loufl=loufl )
+        measurement= createMeasurement( gmean, sigma, leff, optfun )        
+        response= trainer.train( measurement, loufl=loufl )
         for itest in range( ntest ):
             print "Test", itest
-            unfold, hTrue, hMeas= rununfoldtest( bininfo, optunf, response,
-                                                 gmean=gmeantest, gsigma=sigma,
-                                                 leff=leff, optfun=optfun )
+            unfold, hTrue, hMeas= unfoldtester.rununfoldtest( tester,
+                                                              measurement,
+                                                              response )
             unfold.PrintTable( cout, hTrue, 2 )
             hReco= unfold.Hreco( 2 )
             nbin= hReco.GetNbinsX()
@@ -228,7 +330,7 @@ def plotPulls( optunf="Bayes", ntest=10, leff=True, loufl=False,
 
 # Plots with varying smearing resolution:
 def featureSizePlots( optunf="Bayes", leff=True, optfun="exp", opttfun="",
-                      loufl=False ):
+                      loufl=False, gmean=-1.0 ):
 
     if opttfun == "":
         opttfun= optfun
@@ -240,22 +342,25 @@ def featureSizePlots( optunf="Bayes", leff=True, optfun="exp", opttfun="",
     canv= TCanvas( "canv", "feature size plots", 600, 800 )
     canv.Divide( 1, 3 )
 
-    gmeantrain= -1.0
-    gmeantest= gmeantrain
+    trainer= Trainer( bininfo, opttfun )
+    # trainer= BTrainer( bininfo, opttfun )
+
+    tester= Tester( bininfo, optfun )
+    unfoldtester= UnfoldTester( optunf )
     for sigma, ipad in [ [ 0.1, 1 ], [ 0.3, 2 ], [ 1.0, 3 ] ]:
-        response= train( bininfo, gmean=gmeantrain, gsigma=sigma, leff=leff,
-                         opttfun=opttfun, loufl=loufl )
-        unfold, hTrue, hMeas= rununfoldtest( bininfo, optunf, response,
-                                             gmean=gmeantest, gsigma=sigma,
-                                             leff=leff,
-                                             optfun=optfun )
+        measurement= createMeasurement( gmean, sigma, leff, optfun )
+        response= trainer.train( measurement, loufl=loufl )
+        unfold, hTrue, hMeas= unfoldtester.rununfoldtest( tester,
+                                                          measurement,
+                                                          response )
         hReco= unfold.Hreco( 2 )
         histos.append( [ hTrue, hMeas, hReco ] )
         canv.cd( ipad )
         hReco.SetTitle( optunf + ", smear mu, s.d.= " +
-                        str(gmeantrain) + ", " + str(sigma) +
+                        str(gmean) + ", " + str(sigma) +
                         ", train: " + funttxt + ", test: " + funtxt )
         hReco.SetYTitle( "Entries" )
+        hReco.SetMinimum( 0.0 )
         hReco.Draw()
         hMeas.Draw( "same" )
         hTrue.SetLineColor( 8 )
